486896c0df7af53c203c4a59d439b67b3af869a9
Justin Vasel <justin.vasel@gmail.com>
2018-04-10 07:36:36 -0400

    Create and define database

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,18 @@ __version__ = '0.2.2'
 
 # -*- coding: utf-8 -*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
 
+app.config.from_pyfile('config.py')
 app.config['VERSION'] = __version__
 
+db = SQLAlchemy(app)
+
+from app import models
 from app import routes
+
+db.create_all()
+db.session.commit()
--- app/config.py
+++ app/config.py
@@ -2,6 +2,13 @@
 __author__ = 'Justin Vasel'
 import os
 
+# Basics
 BASEDIR      = os.path.abspath(os.path.dirname(__file__))
-DEBUG        = True
+
+# DATABASE
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'data/app.db')
+SQLALCHEMY_ECHO         = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Google Analytics
 GATRACKINGID = 'UA-109768490-2'
