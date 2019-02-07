facae64be4f0b2d265bd9f785467c4dd221df58b
Justin Vasel <justin.vasel@gmail.com>
2019-02-07 12:02:21 -0500

    Add flask-admin interface and overhaul database.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, redirect, request
-from app import app, models, db
+from flask import render_template, redirect, request, url_for
+from flask_login import current_user, login_user, logout_user
+from app import app, models, db, forms
 from datetime import datetime
 
 # - - - Routes - - -
@@ -31,9 +32,8 @@ def cv():
 
 @app.route('/events')
 def events():
-    upcoming_events = db.session.query(models.Event, models.EventType).join(models.EventType, models.EventType.id == models.Event.type).filter(models.Event.date >= datetime.utcnow()).order_by(models.Event.date.asc()).all()
-
-    past_events = db.session.query(models.Event, models.EventType).join(models.EventType, models.EventType.id == models.Event.type).filter(models.Event.date < datetime.utcnow()).order_by(models.Event.date.desc()).all()
+    upcoming_events = models.Event.query.filter(models.Event.date >= datetime.utcnow()).order_by(models.Event.date.asc()).all()
+    past_events     = models.Event.query.filter(models.Event.date < datetime.utcnow()).order_by(models.Event.date.desc()).all()
 
     return render_template('events.html', title='Events', upcoming_events=upcoming_events, past_events=past_events)
 
@@ -41,6 +41,26 @@ def events():
 def research():
     return render_template('research.html', title='Research')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.LoginForm()
+
+    if form.validate_on_submit():
+        user = models.User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 # ERROR HANDLING
