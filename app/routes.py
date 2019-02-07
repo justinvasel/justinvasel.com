c2052941acb7b3366d4621a38850ac722bdb6ad1
Justin Vasel <justin.vasel@gmail.com>
2019-02-07 18:52:46 +0000

    Remove non-existent form field from login route

--- app/routes.py
+++ app/routes.py
@@ -55,7 +55,7 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
+        login_user(user)
         return redirect(url_for('index'))
 
     return render_template('login.html', title='Sign In', form=form)
